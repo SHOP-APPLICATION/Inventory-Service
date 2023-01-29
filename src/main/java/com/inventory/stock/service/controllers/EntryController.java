6faaef4eec220559a7d130ad447338994a48f4d1
ACHAGHOUR ZAKARIA <achaghourzakaria@gmail.com>
2023-01-29 15:25:53 +0100

    WIP: resilience steps

--- src/main/java/com/inventory/stock/service/controllers/EntryController.java
+++ src/main/java/com/inventory/stock/service/controllers/EntryController.java
@@ -3,6 +3,9 @@ package com.inventory.stock.service.controllers;
 import com.inventory.stock.service.dto.EntryRequest;
 import com.inventory.stock.service.dto.EntryResponse;
 import com.inventory.stock.service.services.EntryService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,9 +25,15 @@ public class EntryController {
     private EntryService entryService;
 
     @PostMapping()
+    @CircuitBreaker(name = "product", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "product")
+    @Retry(name = "product")
     public ResponseEntity<EntryResponse> save(@Valid @RequestBody EntryRequest requestDTO){
         log.info("controller Entries : start");
         return new ResponseEntity<>(entryService.addEntry(requestDTO), HttpStatus.CREATED);
+    }
+    public ResponseEntity fallbackMethod(EntryRequest requestDTO, RuntimeException runtimeException) {
+        return new ResponseEntity<>("Somthing went wrong please try again in some seconds",HttpStatus.BAD_REQUEST);
     }
 
     @PutMapping("/{id}")
